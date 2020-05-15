32ac73a4196cca8a77e2727928adf465de25fcb4
G4BK4 <vanzxs@mail.ru>
2020-05-15 16:35:48 +0700

    Make some changes in graph.c and main_graph_test.c

--- graph.h
+++ graph.h
@@ -1,13 +1,20 @@
+#ifndef GRAPH
+#define GRAPH
+#define MAX 100000
+#define V 6
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 struct graph {
     unsigned int nversh; // Число вершин
-    unsigned int* mat; // Матрица n x n
-    unsigned int* visited;
-    char* name[10];
+    unsigned int (*mat)[V]; // Матрица n x n
+    char* name[V];
 };
 
 void graph_clear(struct graph* g);
 void graph_set(struct graph* g);
 struct graph* graph_create(unsigned int nversh);
 void graph_free(struct graph* g);
-void graph_set_edge(struct graph* g, int i, int j, unsigned int weight);
-int graph_get_edge(struct graph* g, int i, int j);
+#endif
